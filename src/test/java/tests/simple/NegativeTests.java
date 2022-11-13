51ab25b8cbbde8e9de6423113dbe48f08d6d010a
Evgenii <sergeevjane@mail.ru>
2022-11-14 00:43:42 +0300

    added fix picture and disabled negative tests

--- src/test/java/tests/simple/NegativeTests.java
+++ src/test/java/tests/simple/NegativeTests.java
@@ -1,36 +1,43 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NegativeTests {
     @Test
+    @Disabled
     void test1() {
         assertTrue(false);
     }
     
     @Test
+    @Disabled
     void test2() {
         assertTrue(false);
     }
 
     @Test
+    @Disabled
     void test3() {
         assertTrue(false);
     }
 
     @Test
+    @Disabled
     void test4() {
         assertTrue(false);
     }
 
     @Test
+    @Disabled
     void test5() {
         assertTrue(false);
     }
 
     @Test
+    @Disabled
     void test6() {
         assertTrue(false);
     }
